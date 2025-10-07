7bef367a9b29e224fbc91f2143f1d783b4ff3558
Soziashvili <okt.davidsoz@gmail.com>
2025-10-07 13:10:54 +0500

    kafka producer correction

--- content_loader_adapter/src/main/java/ru/realtorcrm/content_loader_adapter/service/KafkaProducerService.java
+++ content_loader_adapter/src/main/java/ru/realtorcrm/content_loader_adapter/service/KafkaProducerService.java
@@ -3,7 +3,6 @@ package ru.realtorcrm.content_loader_adapter.service;
 import lombok.AllArgsConstructor;
 import ru.realtorcrm.content_loader_adapter.entity.Building;
 import ru.realtorcrm.content_loader_adapter.entity.BuildingData;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Service
@@ -42,18 +40,8 @@ public class KafkaProducerService {
                      kafkaTemplate.send(BUILDINGS_TOPIC, key, building);
 
             future.thenAccept(System.out::println);
-//            future.whenComplete((result, ex) -> {
-//                if (ex == null) {
-//                    System.out.println("Sent message=[" + source +
-//                            "] with offset=[" + result.getRecordMetadata().offset() + "]");
-//                } else {
-//                    System.out.println("Unable to send message=[" +
-//                            source + "] due to : " + ex.getMessage());
-//                }
-//            });
         }
 
-        // Ждем завершения отправки батча
         try {
             kafkaTemplate.flush();
         } catch (Exception e) {
@@ -62,22 +50,7 @@ public class KafkaProducerService {
     }
 
     private String generatePartitionKey(Building building, String source) {
-        // Генерация ключа для распределения по партициям
-        // Можно использовать разные стратегии в зависимости от требований
-
-        // Пример 1: По источнику данных
-        // return source;
-
-        // Пример 2: По району
-        // return building.getDistrict() != null ? building.getDistrict() : "unknown";
-
-        // Пример 3: Хэш от адреса
         String address = building.getAddress() != null ? building.getAddress() : "unknown";
-        return String.valueOf(Math.abs(address.hashCode() % 10)); // 10 партиций
-    }
-
-    public void sendSync(Building building, String source) throws ExecutionException, InterruptedException {
-        String key = generatePartitionKey(building, source);
-        kafkaTemplate.send(BUILDINGS_TOPIC, key, building).get();
+        return String.valueOf(Math.abs(address.hashCode() % 10));
     }
 }
